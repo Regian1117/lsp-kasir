250ef163820c8c9222ffb368c6047c7c0a5709eb
Regian <158754932+Regian1117@users.noreply.github.com>
2025-09-18 10:23:33 +0700

    sql dan index pesanan

--- admin/pesanan/index.php
+++ admin/pesanan/index.php
@@ -1,234 +1,33 @@
 <?php
-// Tambah ke keranjang
-if (isset($_POST['keranjang'])) {
-    $id_menu = (int) $_POST['id'];
-    $jumlah  = (int) $_POST['jumlah'];
+$stmt = $koneksi->prepare("select * from transaksi");
+$stmt->execute();
+$result = $stmt->get_result();
 
-    if ($jumlah > 0) {
-        // Ambil data menu dari DB
-        $stmt = $koneksi->prepare("SELECT * FROM menu WHERE id = ?");
-        $stmt->bind_param("i", $id_menu);
-        $stmt->execute();
-        $menu = $stmt->get_result()->fetch_assoc();
-
-        if ($menu) {
-            // Cek apakah item sudah ada di keranjang
-            $found = false;
-            if (!empty($_SESSION['cart'])) {
-                foreach ($_SESSION['cart'] as &$c) {
-                    if ($c['id_menu'] == $id_menu) {
-                        $c['jumlah'] += $jumlah;
-                        $c['subtotal'] = $c['harga'] * $c['jumlah'];
-                        $found = true;
-                        break;
-                    }
-                }
-            }
-
-            // Kalau belum ada, tambah baru
-            if (!$found) {
-                $_SESSION['cart'][] = [
-                    'id_menu'   => $menu['id'],
-                    'nama_menu' => $menu['nama_menu'],
-                    'harga'     => $menu['harga'],
-                    'jumlah'    => $jumlah,
-                    'subtotal'  => $menu['harga'] * $jumlah
-                ];
-            }
-        }
-    }
-
-    header("Location: index.php?page=pesanan");
-    exit();
-}
-
-// Kurangi item
-if (isset($_POST['kurangi'])) {
-    $id_menu = (int) $_POST['id_menu'];
-    foreach ($_SESSION['cart'] as $key => &$c) {
-        if ($c['id_menu'] == $id_menu) {
-            $c['jumlah']--;
-            if ($c['jumlah'] <= 0) {
-                unset($_SESSION['cart'][$key]); // hapus kalau jumlah 0
-            } else {
-                $c['subtotal'] = $c['harga'] * $c['jumlah'];
-            }
-            break;
-        }
-    }
-    header("Location: index.php?page=pesanan");
-    exit();
-}
-
-// Hapus item langsung
-if (isset($_POST['hapus'])) {
-    $id_menu = (int) $_POST['id_menu'];
-    foreach ($_SESSION['cart'] as $key => $c) {
-        if ($c['id_menu'] == $id_menu) {
-            unset($_SESSION['cart'][$key]);
-            break;
-        }
-    }
-    header("Location: index.php?page=pesanan");
-    exit();
-}
-
-// Tambah jumlah langsung dari keranjang
-if (isset($_POST['tambah'])) {
-    $id_menu = (int) $_POST['id_menu'];
-    foreach ($_SESSION['cart'] as &$c) {
-        if ($c['id_menu'] == $id_menu) {
-            $c['jumlah']++;
-            $c['subtotal'] = $c['harga'] * $c['jumlah'];
-            break;
-        }
-    }
-    header("Location: index.php?page=pesanan");
-    exit();
-}
-
-if (isset($_POST['checkout'])) {
-    $id_transaksi = $_POST['id_transaksi'];
-    $stmt = $koneksi->prepare("insert into pesanan (id_transaksi,id_menu,jumlah,harga,subtotal) values (?,?,?,?,?)");
-    foreach ($_SESSION['cart'] as $c) {
-        $stmt->bind_param('siiii', $id_transaksi, $c["id_menu"], $c['jumlah'], $c['harga'], $c['subtotal']);
-        $stmt->execute();
-    }
-    $stmt->close();
-    unset($_SESSION['cart']);
-
-    // update total table transaksi
-
-    // ambil total harga dari db
-    $stmt = $koneksi->prepare('select sum(subtotal) total from pesanan where id_transaksi = ?');
-    $stmt->bind_param('i', $id_transaksi);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $row = $result->fetch_assoc();
-    $total = $row['total'] ?? 0;
-
-    //update transaksi
-    $stmt = $koneksi->prepare('update transaksi set total=? where id =? ');
-    $stmt->bind_param('ii', $total, $id_transaksi);
-    $stmt->execute();
-    $stmt->close();
-}
-
-
-// Fungsi ambil menu
-function getMenu($kategori)
+function getPesanan($koneksi, $id)
 {
-    global $koneksi;
-    $stmt = $koneksi->prepare("SELECT * FROM menu WHERE kategori=?");
-    $stmt->bind_param("s", $kategori);
+    $stmt = $koneksi->prepare("select p.*, mn.*, t.total from pesanan p join menu mn on p.id_menu=mn.id join transaksi t on t.id=p.id_transaksi where t.id=?");
+    $stmt->bind_param('i', $id);
     $stmt->execute();
     return $stmt->get_result();
 }
-$makanan = getMenu('makanan');
-$minuman = getMenu('minuman');
 ?>
 
-<div class="flex">
-    <div class="w-2/3 mt-5 mx-5">
-        <!-- Makanan -->
-        <h1 class="text-3xl my-5">Makanan</h1>
-        <div class="wrapper flex gap-4">
-            <?php while ($row = $makanan->fetch_assoc()): ?>
-                <!-- card -->
-                <div class="w-60 bg-white rounded-lg shadow-md flex flex-col gap-2 p-2">
-                    <img src="<?= $row['foto'] ?>" alt="foto makanan" class="w-full h-40 object-cover">
-                    <div class="">
-                        <h3 class="text-xl"><?= $row['nama_menu'] ?></h3>
-                        <h2 class="text-lg"><?= number_format($row['harga'], 0, ',', '.') ?></h2>
-                        <p class="text-xs">stok : <?= $row['stok'] ?></p>
+<div class="flex flex-col gap-4">
+    <?php while ($row = $result->fetch_assoc()): ?>
+        <div class="border p-4">
+            <h1>meja : <?= $row['id_meja'] ?> Nama : <?= $row['nama_pelanggan'] ?></h1>
+            <?php $hasil = getPesanan($koneksi, $row['id']); ?>
+            <div class="flex gap-3">
+                <?php while ($row = $hasil->fetch_assoc()): ?>
+                    <div class="p-2 border">
+                        <img src="<?=$row['foto']?>" class="w-60 h-40 bg-fill">
+                        <h1><?=$row['nama_menu']?></h1>
+                        <h3><?=$row['harga']?> x <?=$row['jumlah']?></h3>
+                        <h3><?=$row['subtotal']?></h3>
+                        
                     </div>
-                    <div class="flex gap-2">
-                        <form method="post" class="inline w-full px-2 py-1 bg-red-400 rounded-lg text-white text-center">
-                            <input type="hidden" name="id" value="<?= $row['id'] ?>">
-                            <input type="number" name="jumlah" placeholder="masukkan jumlah" value=1>
-                            <button name="keranjang">Tambah</button>
-                        </form>
-
-                    </div>
-                </div>
-            <?php endwhile; ?>
+                <?php endwhile; ?>
+            </div>
         </div>
-        <h1 class="text-3xl my-5">Minuman</h1>
-        <div class="wrapper flex gap-4">
-            <?php while ($row = $minuman->fetch_assoc()): ?>
-                <!-- card -->
-                <div class="w-60 bg-white rounded-lg shadow-md flex flex-col gap-2 p-2">
-                    <img src="<?= $row['foto'] ?>" alt="foto minuman" class="w-full h-40 object-cover">
-                    <div class="">
-                        <h3 class="text-xl"><?= $row['nama_menu'] ?></h3>
-                        <h2 class="text-lg"><?= number_format($row['harga'], 0, ',', '.') ?></h2>
-                        <p class="text-xs">stok : <?= $row['stok'] ?></p>
-                    </div>
-                    <div class="flex gap-2">
-                        <form method="post" class="inline w-full px-2 py-1 bg-red-400 rounded-lg text-white text-center">
-                            <input type="hidden" name="id" value="<?= $row['id'] ?>">
-                            <input type="number" name="jumlah" placeholder="masukkan jumlah" value=1>
-                            <button name="keranjang">Tambah</button>
-                        </form>
-
-                    </div>
-                </div>
-            <?php endwhile; ?>
-        </div>
-    </div>
-
-    <!-- keranjang -->
-    <div class="w-1/3 bg-gray-100 p-4 rounded-lg">
-        <h2 class="text-xl font-bold mb-4">Keranjang</h2>
-        <?php if (!empty($_SESSION['cart'])): ?>
-            <ul class="space-y-2">
-                <?php $total = 0;
-                foreach ($_SESSION['cart'] as $c): ?>
-                    <li class="flex justify-between items-center bg-white p-2 rounded shadow">
-                        <div>
-                            <span class="font-semibold"><?= $c['nama_menu'] ?></span>
-                            <span class="text-sm text-gray-600">x <?= $c['jumlah'] ?></span>
-                            <p class="text-xs text-gray-500">Rp <?= number_format($c['harga'], 0, ',', '.') ?> / pcs</p>
-                        </div>
-                        <div class="flex items-center gap-1">
-                            <!-- Kurangi -->
-                            <form method="post">
-                                <input type="hidden" name="id_menu" value="<?= $c['id_menu'] ?>">
-                                <button type="submit" name="kurangi" class="px-2 bg-yellow-400 text-white rounded">-</button>
-                            </form>
-                            <!-- Tambah -->
-                            <form method="post">
-                                <input type="hidden" name="id_menu" value="<?= $c['id_menu'] ?>">
-                                <button type="submit" name="tambah" class="px-2 bg-green-500 text-white rounded">+</button>
-                            </form>
-                            <!-- Hapus -->
-                            <form method="post">
-                                <input type="hidden" name="id_menu" value="<?= $c['id_menu'] ?>">
-                                <button type="submit" name="hapus" class="px-2 bg-red-500 text-white rounded">x</button>
-                            </form>
-                            <!-- Subtotal -->
-                            <span class="ml-2 font-semibold">Rp <?= number_format($c['subtotal'], 0, ',', '.') ?></span>
-                        </div>
-                    </li>
-                    <?php $total += $c['subtotal']; ?>
-                <?php endforeach; ?>
-            </ul>
-            <form method="post" class="flex flex-col mt-2">
-                <select name="id_transaksi" id="idtr">
-                    <?php
-                    $stmt = $koneksi->prepare("select t.id, t.kode_transaksi, m.nomor_meja from transaksi t join meja m on m.id=t.id_meja");
-                    $stmt->execute();
-                    $result = $stmt->get_result();
-                    while ($row = $result->fetch_assoc()) {
-                    ?>
-                        <option value="<?= $row['id'] ?>">kode : <?= $row['kode_transaksi'] ?> meja : <?= $row['nomor_meja'] ?></option>
-                    <?php } ?>
-                </select>
-                <div class="mt-1 font-bold">Total: Rp <?= number_format($total, 0, ',', '.') ?></div>
-                <button type="submit" name="checkout" class="w-1/3 mt-2 px-4 py-2 bg-blue-500 text-white rounded">Checkout</button>
-            </form>
-        <?php else: ?>
-            <p>Keranjang kosong</p>
-        <?php endif; ?>
-    </div>
+    <?php endwhile; ?>
 </div>
---
+++ restoran (1).sql
@@ -0,0 +1,250 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 18 Sep 2025 pada 04.22
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `restoran`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `meja`
+--
+
+CREATE TABLE `meja` (
+  `id` int(11) NOT NULL,
+  `nomor_meja` int(10) NOT NULL,
+  `status` enum('kosong','terisi','reserved') DEFAULT 'kosong'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `meja`
+--
+
+INSERT INTO `meja` (`id`, `nomor_meja`, `status`) VALUES
+(1, 1, 'terisi'),
+(2, 2, 'kosong'),
+(3, 3, 'kosong'),
+(4, 4, 'terisi'),
+(5, 5, 'terisi'),
+(6, 6, 'kosong'),
+(7, 7, 'kosong'),
+(8, 8, 'kosong'),
+(13, 28, 'reserved'),
+(14, 69, 'terisi');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `menu`
+--
+
+CREATE TABLE `menu` (
+  `id` int(11) NOT NULL,
+  `nama_menu` varchar(100) NOT NULL,
+  `kategori` enum('makanan','minuman') NOT NULL,
+  `harga` int(10) NOT NULL,
+  `stok` int(11) DEFAULT 0,
+  `foto` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `menu`
+--
+
+INSERT INTO `menu` (`id`, `nama_menu`, `kategori`, `harga`, `stok`, `foto`) VALUES
+(1, 'Indomi Goreng', 'makanan', 10000, 6767, 'uploads/indomie-goreng.jpg'),
+(2, 'Bebek Goreng Madura', 'makanan', 22000, 13, 'uploads/bebek-madura.jpg'),
+(3, 'pizza', 'makanan', 89000, 89898, 'uploads/646_pizza.jpg'),
+(4, 'Dawet', 'minuman', 10000, 10101, 'uploads/439_Dawet.jpg'),
+(5, 'sahorni', 'minuman', 696969, 1, 'uploads/644_sahorni.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `pesanan`
+--
+
+CREATE TABLE `pesanan` (
+  `id` int(11) NOT NULL,
+  `id_transaksi` int(11) NOT NULL,
+  `id_menu` int(11) NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `harga` int(10) NOT NULL,
+  `subtotal` int(12) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `pesanan`
+--
+
+INSERT INTO `pesanan` (`id`, `id_transaksi`, `id_menu`, `jumlah`, `harga`, `subtotal`) VALUES
+(1, 1, 2, 1, 22000, 22000),
+(2, 1, 4, 1, 10000, 10000),
+(3, 1, 1, 1, 10000, 10000),
+(4, 2, 5, 1, 696969, 696969),
+(5, 2, 4, 50, 10000, 500000),
+(6, 1, 2, 1, 22000, 22000);
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` int(11) NOT NULL,
+  `kode_transaksi` varchar(20) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_meja` int(11) DEFAULT NULL,
+  `total` int(12) NOT NULL,
+  `status` enum('pending','paid','cancel') DEFAULT 'pending',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `nama_pelanggan` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `transaksi`
+--
+
+INSERT INTO `transaksi` (`id`, `kode_transaksi`, `id_user`, `id_meja`, `total`, `status`, `created_at`, `nama_pelanggan`) VALUES
+(1, 'TRX20250914-001', 4, 4, 64000, 'pending', '2025-09-14 06:49:38', 'The King'),
+(2, 'TRX20250914-002', 4, 5, 1196969, 'pending', '2025-09-14 07:33:10', 'arka');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role` enum('admin','waiter','kasir','owner') NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `users`
+--
+
+INSERT INTO `users` (`id`, `nama`, `username`, `password`, `role`, `created_at`) VALUES
+(1, 'admin gokil', 'admin', 'admin', 'admin', '2025-09-11 07:48:50'),
+(2, 'owner gokil', 'owner', 'owner', 'owner', '2025-09-11 12:29:40'),
+(3, 'waiter gokil', 'waiter', 'waiter', 'waiter', '2025-09-11 13:21:30'),
+(4, 'kasir gokil', 'kasir', 'kasir', 'kasir', '2025-09-11 13:21:30');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `meja`
+--
+ALTER TABLE `meja`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `menu`
+--
+ALTER TABLE `menu`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_transaksi` (`id_transaksi`),
+  ADD KEY `id_menu` (`id_menu`);
+
+--
+-- Indeks untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `kode_transaksi` (`kode_transaksi`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_meja` (`id_meja`);
+
+--
+-- Indeks untuk tabel `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT untuk tabel yang dibuang
+--
+
+--
+-- AUTO_INCREMENT untuk tabel `meja`
+--
+ALTER TABLE `meja`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT untuk tabel `menu`
+--
+ALTER TABLE `menu`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT untuk tabel `pesanan`
+--
+ALTER TABLE `pesanan`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD CONSTRAINT `pesanan_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id`),
+  ADD CONSTRAINT `pesanan_ibfk_2` FOREIGN KEY (`id_menu`) REFERENCES `menu` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`),
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_meja`) REFERENCES `meja` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
